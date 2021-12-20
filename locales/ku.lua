1b5ef5af111960f7c1ce9ca95a5e9c80eee4e142
Kurdish Dev <89542631+shexaaa@users.noreply.github.com>
2021-12-20 12:22:43 +0300

    Add files via upload

---
+++ locales/ku.lua
@@ -0,0 +1,8 @@
+Locales ['ku'] = {
+  ['writingcontract'] = 'لە بواری جێبەچی کردندایە: %s',
+  ['soldvehicle'] = 'فرۆشرا ~r~%s~s~',
+  ['boughtvehicle'] = 'کڕدڕا ~g~%s~s~',
+  ['notyourcar'] = 'ئەم سەیارە هیتۆنیە',
+  ['nonearby'] = 'هیچ سەیارەیەکت لە لا نیە',
+  ['nonearbybuyer'] = 'کڕیارت لە لا نیە',
+}
